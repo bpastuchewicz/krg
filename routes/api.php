559a867f3f24b981940a1d2bac6029274d9f406b
Bartosz Pastuchewicz <bpastuchewicz@gmail.com>
2023-04-22 18:51:06 +0200

    Cleanup

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,6 @@
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\ExchangeRateController;
 use App\Http\Controllers\Api\PostController;
-use App\Http\Middleware\Roles;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,13 +16,6 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-//Route::apiResource('posts', PostController::class)->middleware('auth:sanctum')->middleware('roles:admin');
-Route::get('/posts', [PostController::class,'index']);
-Route::post('/posts',[PostController::class, 'store'])->middleware('auth:sanctum')->middleware('roles:admin');
-Route::put('/posts/{id}',[PostController::class, 'update'])->middleware('auth:sanctum')->middleware('roles:admin');
-Route::get('/posts/{id}', [PostController::class,'show']);
-Route::delete('/posts/{id}',[PostController::class,'destroy'])->middleware('auth:sanctum')->middleware('roles:admin');
-
 Route::get('/exchange_rates',[ExchangeRateController::class, 'index'])->middleware('auth:sanctum');
 Route::get('/exchange_rates/{date}',[ExchangeRateController::class, 'index'])->middleware('auth:sanctum');
 Route::get('/exchange_rates/{currency}/{date}',[ExchangeRateController::class, 'show'])->middleware('auth:sanctum');
